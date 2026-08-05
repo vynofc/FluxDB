247fab09058c062073848d1499c72e14f6d2619b
niliees <nils.privat27@gmail.com>
2026-08-05 11:11:26 +0200

    removed duplicat lines

--- WPF/FluxDB/Services/ExportService.cs
+++ WPF/FluxDB/Services/ExportService.cs
@@ -25,41 +25,7 @@ namespace FluxDB.Services
             using (var writer = new StreamWriter(filePath, false, Encoding.UTF8))
             using (var jsonWriter = new JsonTextWriter(writer) { Formatting = Formatting.Indented })
             {
-                jsonWriter.WriteStartObject();
-                jsonWriter.WritePropertyName("version");
-                jsonWriter.WriteValue("1.0");
-                jsonWriter.WritePropertyName("exportedAt");
-                jsonWriter.WriteValue(DateTime.Now.ToString("o"));
-                jsonWriter.WritePropertyName("rootFolder");
-                jsonWriter.WriteValue(rootFolder);
-                jsonWriter.WritePropertyName("totalFiles");
-                jsonWriter.WriteValue(files.Count);
-                jsonWriter.WritePropertyName("files");
-                jsonWriter.WriteStartArray();
-
-                foreach (var file in files)
-                {
-                    jsonWriter.WriteStartObject();
-                    jsonWriter.WritePropertyName("path"); jsonWriter.WriteValue(file.Path);
-                    jsonWriter.WritePropertyName("name"); jsonWriter.WriteValue(file.Name);
-                    jsonWriter.WritePropertyName("extension"); jsonWriter.WriteValue(file.Extension);
-                    jsonWriter.WritePropertyName("createdAt"); jsonWriter.WriteValue(file.CreatedAt);
-                    jsonWriter.WritePropertyName("modifiedAt"); jsonWriter.WriteValue(file.ModifiedAt);
-                    jsonWriter.WritePropertyName("size"); jsonWriter.WriteValue(file.Size);
-
-                    jsonWriter.WritePropertyName("tags");
-                    jsonWriter.WriteStartArray();
-                    if (file.Tags != null)
-                        foreach (var tag in file.Tags)
-                            jsonWriter.WriteValue(tag);
-                    jsonWriter.WriteEndArray();
-
-                    jsonWriter.WritePropertyName("note"); jsonWriter.WriteValue(file.Note ?? "");
-                    jsonWriter.WriteEndObject();
-                }
-
-                jsonWriter.WriteEndArray();
-                jsonWriter.WriteEndObject();
+                WriteExport(jsonWriter, files, rootFolder);
             }
         }
 
@@ -71,42 +37,47 @@ namespace FluxDB.Services
             using (var writer = new StreamWriter(gzipStream, Encoding.UTF8))
             using (var jsonWriter = new JsonTextWriter(writer))
             {
+                WriteExport(jsonWriter, files, rootFolder);
+            }
+        }
+
+        private void WriteExport(JsonTextWriter jsonWriter, List<FileEntry> files, string rootFolder)
+        {
+            jsonWriter.WriteStartObject();
+            jsonWriter.WritePropertyName("version");
+            jsonWriter.WriteValue("1.0");
+            jsonWriter.WritePropertyName("exportedAt");
+            jsonWriter.WriteValue(DateTime.Now.ToString("o"));
+            jsonWriter.WritePropertyName("rootFolder");
+            jsonWriter.WriteValue(rootFolder);
+            jsonWriter.WritePropertyName("totalFiles");
+            jsonWriter.WriteValue(files.Count);
+            jsonWriter.WritePropertyName("files");
+            jsonWriter.WriteStartArray();
+
+            foreach (var file in files)
+            {
                 jsonWriter.WriteStartObject();
-                jsonWriter.WritePropertyName("version");
-                jsonWriter.WriteValue("1.0");
-                jsonWriter.WritePropertyName("exportedAt");
-                jsonWriter.WriteValue(DateTime.Now.ToString("o"));
-                jsonWriter.WritePropertyName("rootFolder");
-                jsonWriter.WriteValue(rootFolder);
-                jsonWriter.WritePropertyName("totalFiles");
-                jsonWriter.WriteValue(files.Count);
-                jsonWriter.WritePropertyName("files");
+                jsonWriter.WritePropertyName("path"); jsonWriter.WriteValue(file.Path);
+                jsonWriter.WritePropertyName("name"); jsonWriter.WriteValue(file.Name);
+                jsonWriter.WritePropertyName("extension"); jsonWriter.WriteValue(file.Extension);
+                jsonWriter.WritePropertyName("createdAt"); jsonWriter.WriteValue(file.CreatedAt);
+                jsonWriter.WritePropertyName("modifiedAt"); jsonWriter.WriteValue(file.ModifiedAt);
+                jsonWriter.WritePropertyName("size"); jsonWriter.WriteValue(file.Size);
+
+                jsonWriter.WritePropertyName("tags");
                 jsonWriter.WriteStartArray();
-
-                foreach (var file in files)
-                {
-                    jsonWriter.WriteStartObject();
-                    jsonWriter.WritePropertyName("path"); jsonWriter.WriteValue(file.Path);
-                    jsonWriter.WritePropertyName("name"); jsonWriter.WriteValue(file.Name);
-                    jsonWriter.WritePropertyName("extension"); jsonWriter.WriteValue(file.Extension);
-                    jsonWriter.WritePropertyName("createdAt"); jsonWriter.WriteValue(file.CreatedAt);
-                    jsonWriter.WritePropertyName("modifiedAt"); jsonWriter.WriteValue(file.ModifiedAt);
-                    jsonWriter.WritePropertyName("size"); jsonWriter.WriteValue(file.Size);
-
-                    jsonWriter.WritePropertyName("tags");
-                    jsonWriter.WriteStartArray();
-                    if (file.Tags != null)
-                        foreach (var tag in file.Tags)
-                            jsonWriter.WriteValue(tag);
-                    jsonWriter.WriteEndArray();
-
-                    jsonWriter.WritePropertyName("note"); jsonWriter.WriteValue(file.Note ?? "");
-                    jsonWriter.WriteEndObject();
-                }
-
+                if (file.Tags != null)
+                    foreach (var tag in file.Tags)
+                        jsonWriter.WriteValue(tag);
                 jsonWriter.WriteEndArray();
+
+                jsonWriter.WritePropertyName("note"); jsonWriter.WriteValue(file.Note ?? "");
                 jsonWriter.WriteEndObject();
             }
+
+            jsonWriter.WriteEndArray();
+            jsonWriter.WriteEndObject();
         }
     }
 }
